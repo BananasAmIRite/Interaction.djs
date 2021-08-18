c6f36ab25ce295c14e0db1711a63dcb22f634ebe
BananasAmIRite <jasony1230@gmail.com>
2021-08-18 12:59:41 -0400

    fix(cold): fixed cold's mistakes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
-export * from './Interactions/CommandInteraction/CommandInteractionHandler';
-export * from './Interactions/CommandInteraction/CommandInteractionManager';
-export * from './InteractionsAPI/BaseInteractionHandler';
-export * from './InteractionsAPI/InteractionEventHandler';
-export * from './InteractionsAPI/Managers/BaseInteractionManager';
+import InteractionEventHandler from './InteractionsAPI/InteractionEventHandler';
+import BaseInteractionHandler from './InteractionsAPI/BaseInteractionHandler';
+import BaseInteractionManager from './InteractionsAPI/BaseInteractionManager';
+import CommandInteractionHandler from './Interactions/CommandInteraction/CommandInteractionHandler';
+import CommandInteractionManager from './Interactions/CommandInteraction/CommandInteractionManager';
+import ButtonInteractionManager from './Interactions/ButtonInteraction/ButtonInteractionManager';
+import ButtonInteractionHandler from './Interactions/ButtonInteraction/ButtonInteractionHandler';
+
+export {
+  // base api
+  BaseInteractionHandler,
+  BaseInteractionManager,
+  // CommandInteraction
+  CommandInteractionHandler,
+  CommandInteractionManager,
+  // ButtonInteraction
+  ButtonInteractionHandler,
+  ButtonInteractionManager,
+};
+export default InteractionEventHandler;
